c7eaa101e04dd889eac3d35c3942a8f61c2b4629
Arun Nair <arun.nair@cygrp.com>
2019-11-08 14:07:21 +0530

    step3

--- index.php
+++ index.php
@@ -5,24 +5,27 @@ if(isset($_POST["email"]))
 	echo "arun";
 	try
 	{
+	
 		require 'class/PHPMailer.php';
 		require 'class/SMTP.php';
-	$mail = new PHPMailer;
-	$mail->SMTPDebug = SMTP::DEBUG_SERVER; 
-	
-	$mail->IsSMTP();
-	$mail->Host = 'smtp.gmail.com';
-	$mail->Port       = 587; 
-	$mail->SMTPAuth = true;
-	$mail->Username = 'codelearnervin@gmail.com';
-	$mail->Password = '9818064641';
-	$mail->SMTPSecure = '';
-	$mail->setFrom = 'codelearnervin@gmail.com';
-	$mail->FromName = 'codelearner';
-	$mail->AddAddress('arun.vin09@gmail.com','Name');
-	$mail->IsHTML(true);
-	$mail->Subject='test';
-	$mail->body = 'test again';
+		$mail = new PHPMailer(true);
+		$mail->SMTPDebug = SMTP::DEBUG_SERVER; 
+		
+		
+		
+		$mail->IsSMTP();
+		$mail->Host = 'smtp.gmail.com';
+		$mail->Port       = 587; 
+		$mail->SMTPAuth = true;
+		$mail->Username = 'codelearnervin@gmail.com';
+		$mail->Password = '9818064641';
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+		$mail->setFrom = 'codelearnervin@gmail.com';
+		$mail->FromName = 'codelearner';
+		$mail->AddAddress('arun.vin09@gmail.com','Name');
+		$mail->IsHTML(true);
+		$mail->Subject='test';
+		$mail->Body = 'test again';
 	
 		$mail->Send();
 		echo "snet";
